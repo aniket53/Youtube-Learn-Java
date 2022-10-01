49960d8d2ec15bbde9af1a6eb5e9347d2a196bc2
Aniket_Khot <65432335+aniket53@users.noreply.github.com>
2022-10-01 15:25:13 +0530

    Add files via upload

---
+++ Code/Exercise1_All_Solutions.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+public class Exercise1_All_Solutions {
+//    #1
+//    public static void main(String[] args) {
+//    //        C=(5(F-32))/9
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Input a degree in Fah.");
+//        double fah =input.nextDouble();
+//
+//        double celsius = ((5*(fah -32.0))/9.0);
+//        System.out.println(fah+" "+ "degree Fah. is equal to "+celsius+" in celsius");
+//
+//    }
+
+//    #2
+    //public static void main(String[] args) {
+    //    Scanner input = new Scanner(System.in);
+    //    double minutesInYear = 60*24*365;
+    //    System.out.println("Input the number of minutes:");
+    //    double min = input.nextDouble();
+    //
+    //    long years =(long) (min/minutesInYear);
+    //    int days =(int) (min/60/24)%365;
+    //
+    //    System.out.println((int) min+" minutes is apporx."+years+" years and "+days+" days");
+    //
+    //}
+
+//    #3
+    //public static void main(String[] args) {
+    //    Scanner input = new Scanner(System.in);
+    //
+    //    System.out.println("Enter your weight in pounds:");
+    //    double weight = input.nextDouble();
+    //
+    //    System.out.println("Enter your height in inches:");
+    //    double inches = input.nextDouble();
+    //
+    //    double BMI = weight * 0.4535/(inches*0.0254*inches*0.0254);
+    //    System.out.println("Your BMI will be: "+BMI);
+
+
+//    #4
+    //public static void main(String[] args) {
+    //    Scanner input = new Scanner(System.in);
+    //    System.out.println("Enter six non negative digits:");
+    //    int inputNumber = input.nextInt(); //123456
+    //    int n1= inputNumber/100000%10; //1
+    //    int n2= inputNumber/10000%10; //2
+    //    int n3= inputNumber/1000%10; //3
+    //    int n4= inputNumber/100%10; //4
+    //    int n5= inputNumber/10%10; //5
+    //    int n6= inputNumber%10; //6
+    //
+    //    System.out.println(n1+" "+n2+" "+n3+" "+n4+" "+n5+" "+n6);
+    //}
+
+//    #5
+    //public static void main(String[] args) {
+    //    Scanner sc = new Scanner(System.in);
+    //    System.out.println("Enter the first number:");
+    //    int firstInt=sc.nextInt();
+    //    System.out.println("Enter the second number:");
+    //    int secondInt=sc.nextInt();
+    //
+    //    int sum=firstInt+secondInt;
+    //    System.out.println("Sum will be: "+sum);
+    //
+    //    int subs =firstInt-secondInt;
+    //    System.out.println("Difference will be: "+subs);
+    //
+    //    int product=firstInt*secondInt;
+    //    System.out.println("Product will be: "+product);
+    //
+    //    int avg=(firstInt+secondInt)/2;
+    //    System.out.println("Average will be: "+avg);
+    //}
+}
