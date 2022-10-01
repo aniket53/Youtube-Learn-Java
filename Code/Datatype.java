851ccbfcdd0133c99056bffc02c59c490fb82699
Aniket_Khot <65432335+aniket53@users.noreply.github.com>
2022-10-01 12:09:36 +0530

    Add files via upload

---
+++ Code/Datatype.java
@@ -0,0 +1,10 @@
+public class Datatype {
+    public static void main(String[] args) {
+//        Boolean one = true;
+//        short s=10000;
+//        short r=-5000;
+//        float f1=2.50f;
+//        int a=250;
+//        System.out.println(a);
+    }
+}
---
+++ Code/Variable.java
@@ -0,0 +1,16 @@
+public class Variable {
+    static int m=100; //static variables
+
+    void method(){
+        int n=90; //local variable
+    }
+    public static void main(String[] args) {
+        int data=50; //instance variable
+
+//        Addition of two numbers
+        int a=10;
+        int b=20;
+        int c=b-a;
+        System.out.println(c);
+    }
+}
