8b641ead667edfd20ea1c76759db34419b465036
Aniket_Khot <65432335+aniket53@users.noreply.github.com>
2022-10-01 17:37:50 +0530

    Add files via upload

---
+++ Code/If_Else.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+public class If_Else {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+//        if
+//        int age = 20;
+//        //checking the age
+//        if (age > 18) {
+//            System.out.print("Age is greater than 18");
+//        }
+
+//        if-else
+//        int year=2020;
+//        if(((year % 4 ==0) && (year % 100 !=0)) || (year % 400==0)){
+//            System.out.println("LEAP YEAR");
+//        }
+//        else{
+//            System.out.println("COMMON YEAR");
+//        }
+
+//        if-else-if ladder
+//        int marks=65;
+//
+//        if(marks<50){
+//            System.out.println("fail");
+//        }
+//        else if(marks>=50 && marks<60){
+//            System.out.println("D grade");
+//        }
+//        else if(marks>=60 && marks<70){
+//            System.out.println("C grade");
+//        }
+//        else if(marks>=70 && marks<80){
+//            System.out.println("B grade");
+//        }
+//        else if(marks>=80 && marks<90){
+//            System.out.println("A grade");
+//        }else if(marks>=90 && marks<100){
+//            System.out.println("A+ grade");
+//        }else{
+//            System.out.println("Invalid!");
+//        }
+
+//        nested if
+//        int age=20;
+//        int weight=80;
+//        //applying condition on age and weight
+//        if(age>=18){
+//            if(weight>50){
+//                System.out.println("You are eligible to donate blood");
+//            }
+//        }
+    }
+}
