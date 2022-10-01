c6b065d662639f65c6d0fa402e13a2f2e5ddeb12
Aniket_Khot <65432335+aniket53@users.noreply.github.com>
2022-10-01 16:18:20 +0530

    Add files via upload

---
+++ Code/Operators.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Operators {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        Unary Ops.
+//        int x=10;
+//        System.out.println(x++);
+//        System.out.println(++x);
+
+//        Arithmetic Ops.
+//        int a=10;
+//        int b=20;
+//        System.out.println(a+b);
+
+//        Left Shift Ops.
+//        System.out.println(10<<2); //10*2^2=10*4=40
+
+//        Right Shift Ops.
+//        System.out.println(10>>2); //10/2^2=10/4=2
+
+//        Logical && and Bitwise &
+//        int a=10;
+//        int b=5;
+//        int c=20;
+//        System.out.println(a<b && a<c); //false && true =false
+//        System.out.println(a<b & a<c); //false & true = false
+
+//        Assignment Ops..
+//        int a=10;
+//        int b=20;
+//        a+=4; //a=a+4 a=10+4=14
+//        b-=4; //b=b-4 b=20-4=16
+//        System.out.println(a);
+//        System.out.println(b);
+    }
+}
