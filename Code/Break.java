f8dd6a12dec61dde9e28185e6ab4eaa35af35b62
Aniket_Khot <65432335+aniket53@users.noreply.github.com>
2022-10-02 17:42:36 +0530

    Add files via upload

---
+++ Code/Break.java
@@ -0,0 +1,20 @@
+public class Break {
+    public static void main(String[] args) {
+//        for(int i=1;i<=10;i++){
+//            if(i==5){
+//                break;
+//            }
+//            System.out.println(i);
+//        }
+//        for(int i=1;i<=10;i++){
+//            if(i==5){
+//                continue;
+//            }
+//            System.out.println(i);
+//        }
+        //this is a single line comment in java..
+
+        /*this is a
+                multiline comment in java..*/
+    }
+}
---
+++ Code/Forloop.java
@@ -0,0 +1,18 @@
+public class Forloop {
+    public static void main(String[] args) {
+//        for(int i=1;i<=10;i++){
+//            System.out.println(i);
+//        }
+//        *
+//        * *
+//        * * *
+//        * * * *
+//        * * * * *
+//        for(int i=1;i<=5;i++){
+//            for(int j=1;j<=i;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+    }
+}
