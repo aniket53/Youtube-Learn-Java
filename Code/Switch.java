5eaff06be20a4801cfe8c8f03db26225e4b8a108
Aniket_Khot <65432335+aniket53@users.noreply.github.com>
2022-10-02 10:44:29 +0530

    Add files via upload

---
+++ Code/Switch.java
@@ -0,0 +1,18 @@
+public class Switch {
+    public static void main(String[] args) {
+        int number=30;
+        switch(number){
+            case 10:
+                System.out.println("10");
+                break;
+
+            case 20:
+                System.out.println("20");
+                break;
+
+            default:
+                System.out.println("Not 10 or 20");
+
+        }
+    }
+}
